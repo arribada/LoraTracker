e54e24961137b79904f5532c76b52b40036b031e
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-08-03 19:12:38 +0300

    full refactoring
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- tools/gpsLora/gpsLora.go
+++ tools/gpsLora/gpsLora.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/hex"
 	"fmt"
+	"github.com/pkg/errors"
 	"log"
 	"strings"
 	"time"
@@ -16,104 +17,134 @@ import (
 func main() {
 	log.SetFlags(log.Ltime | log.Lshortfile)
 
+	respChan, err := enableGPS()
+	if err != nil {
+		log.Fatal("failed to enable gps err:", err)
+	}
+
+	lora, err := newLoraConnection()
+	if err != nil {
+		log.Fatal("failed to create lora connection err:", err)
+	}
+
+	for {
+		dataGPS := <-respChan
+		dataLora := hex.EncodeToString([]byte(dataGPS.Time.String()))
+		fmt.Println("sent data", dataLora, dataGPS)
+
+		fmt.Println(lora)
+
+		resp, err := lora.Send("0,1," + dataLora)
+		if err != nil || resp != "OK" {
+			log.Fatalf("failed to send data err:%v resp:%v \n", err, resp)
+		}
+	}
+}
+
+func enableGPS() (chan nmea.RMC, error) {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600, ReadTimeout: 3000 * time.Second}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		return nil, errors.Wrap(err, "enable port")
+	}
+
+	reader := bufio.NewReader(s.Reader())
+
+	// Full ref: https://cdn-shop.adafruit.com/datasheets/PMTK_A08.pdf
+	// Turn on just minimum info (RMC only, location):
+	command := "PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+	s.Write([]byte("$" + command + "*" + nmea.XORChecksum(command) + "\r\n"))
+
+	if !gerMTKAck(314, reader) {
+		return nil, errors.New("no cmd ack")
+	}
+
+	// Set update rate to once every 10 second (10hz).
+	command = "PMTK220,10000"
+	s.Write([]byte("$" + command + "*" + nmea.XORChecksum(command) + "\r\n"))
+	if !gerMTKAck(220, reader) {
+		return nil, errors.New("no cmd ack")
+	}
+
+	respChan := make(chan nmea.RMC)
+	go func() {
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			parsed, err := nmea.Parse(strings.TrimSpace(line))
+			if err != nil {
+				log.Fatal(err)
+			}
+			if parsed.DataType() == nmea.TypeRMC {
+				data := parsed.(nmea.RMC)
+				respChan <- data
+			}
+		}
+	}()
+
+	return respChan, nil
+}
+
+func newLoraConnection() (*rak811.Lora, error) {
 	cfg := &serial.Config{
 		Name:        "/dev/ttyAMA0",
 		ReadTimeout: 20000 * time.Millisecond,
 	}
 	lora, err := rak811.New(cfg)
 	if err != nil {
-		log.Fatal(err, "failed to create rak811 instance")
+		return nil, errors.Wrap(err, "create rak811 instance")
 	}
+	log.Println("lora module initialized")
 
 	if err := lora.HardReset(); err != nil {
-		log.Fatal("failed to reset module err:", err)
+		return nil, errors.Wrap(err, "reset module")
 	}
+	log.Println("lora module reset")
+
 
 	resp, err := lora.SetMode(0)
-	if err != nil {
-		log.Fatal("failed to set mode err:", err)
+	if err != nil{
+		return nil, errors.Wrapf(err, "set lora mod")
 	}
-	fmt.Println("set mode", resp)
+	log.Println("lora module mode set",resp)
 
 	resp, err = lora.SetConfig("nwks_key:01020304050607080910111213141516&dev_eui:3038383664388108&app_key:01020304050607080910111213141516&app_eui:0000010000000000")
-	if err != nil {
-		log.Fatal("failed to set config err:", err)
+	if err != nil{
+		return nil, errors.Wrapf(err, "set lora config")
 	}
-	fmt.Println("config", resp)
+	log.Println("lora module config set",resp)
+
 
 	resp, err = lora.JoinOTAA()
 	if err != nil {
-		log.Fatal("failed to join:", err)
+		return nil, errors.Wrapf(err, "lora join")
 	}
-	fmt.Println("join", resp)
+	log.Println("lora module joined",resp)
 
-	for index := 0; index < 10; index++ {
-
-		gpsD, err := readGPS()
-		if err != nil {
-			fmt.Println("error reading gps", resp)
-		}
-
-		data := hex.EncodeToString([]byte(gpsD.Time.String()))
-		resp, err = lora.Send("0,1," + data)
-		if err != nil {
-			log.Fatal("failed to send data:", err)
-		}
-		fmt.Println("send", resp)
-
-		time.Sleep(10 * time.Second)
-
-	}
-
+	return lora, nil
 }
 
-func readGPS() (*nmea.RMC, error) {
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600, ReadTimeout: 10000 * time.Second}
-	s, err := serial.OpenPort(c)
-	if err != nil {
-		log.Fatal(err)
+// gerCmdAck reads untill it gets an ack for the initial setup command or
+// untill it reached a reader error.
+// This ic because the module might be currenlty active so
+// might receive another response before the ack recponse.
+func gerMTKAck(cmdID int, reader *bufio.Reader) bool {
+	var ok bool
+	for x := 0; x < 20; x++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		resp, err := nmea.Parse(strings.TrimSpace(line))
+		if resp.TalkerID() == nmea.TypeMTK {
+			d := resp.(nmea.MTK)
+			if d.Cmd == cmdID && d.Flag == 3 {
+				ok = true
+				break
+			}
+		}
 	}
-
-	s.Write([]byte("b'$'"))
-	// Turn on just minimum info (RMC only, location):
-	s.Write([]byte("b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'"))
-	s.Write([]byte("b'*'"))
-	s.Write([]byte("b'29'"))
-	s.Write([]byte("b'\r\n'"))
-	s.Write([]byte("b'$'"))
-	// Set update rate to once a second (1hz) which is what you typically want.
-	s.Write([]byte("b'PMTK220,1000'"))
-	s.Write([]byte("b'*'"))
-	s.Write([]byte("b'1F'"))
-	s.Write([]byte("b'\r\n'"))
-
-	reader := bufio.NewReader(s.Reader())
-
-	defer s.Flush()
-	defer s.Close()
-
-	for {
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println(strings.TrimSpace(line))
-
-	ss, err := nmea.Parse(strings.TrimSpace(line))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if ss.DataType() == nmea.TypeRMC {
-		data := ss.(nmea.RMC)
-
-		fmt.Println(data)
-
-
-		// return &data, nil
-	}
-}
-
-return nil, fmt.Errorf("invalid data")
+	return ok
 }
