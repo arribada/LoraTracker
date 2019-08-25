ffe42e4a626d75e0ec0bd7d90d79dcf7b823f646
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-08-25 17:37:21 +0300

    gps sender cli - refactoring
    use env variables instead of flags so that can set them on the balena
    gui.
    using the forked rak811 version as the maintainer want to use with a
    different approach which I believe is wrong.
    added lots of debug logs
    
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- tools/GPSSender/go.mod
+++ tools/GPSSender/go.mod
@@ -6,18 +6,11 @@ require (
 	github.com/adrianmo/go-nmea v1.1.1-0.20190809134752-fb3e95815d06
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190717042225-c3de453c63f4 // indirect
-	github.com/calvernaz/rak811 v0.0.0-20190818143136-ba4d56e1fb47
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/krasi-georgiev/rak811 v0.0.0-20190824160524-3ad4a453bd96
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
-	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
-
-// replace github.com/adrianmo/go-nmea => ../../../../adrianmo/go-nmea
-
-// replace github.com/calvernaz/rak811 => ../../../../calvernaz/rak811
-
-// replace github.com/calvernaz/rak811 => github.com/krasi-georgiev/rak811 v0.0.0-20190818134618-3c78ecab2f81
--- tools/GPSSender/main.go
+++ tools/GPSSender/main.go
@@ -12,67 +12,141 @@ import (
 
 	"github.com/pkg/errors"
 
-	"path/filepath"
-
 	"github.com/adrianmo/go-nmea"
-	"github.com/calvernaz/rak811"
+	"github.com/krasi-georgiev/rak811"
 	"github.com/tarm/serial"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
 	log.SetFlags(log.Ltime | log.Lshortfile)
-	app := kingpin.New(filepath.Base(os.Args[0]), "A tool that connects to the Adafruit USB GPS board and sends the data to a RAK811 module")
-	app.HelpFlag.Short('h')
 
-	devEUI := app.Flag("dev_eui", "lora server dev_eui").
-		Required().
-		String()
-	appKey := app.Flag("app_key", "lora server app_key").
-		Required().
-		String()
-	debug := app.Flag("debug", "show debug logs").
-		Bool()
-
-	if _, err := app.Parse(os.Args[1:]); err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
-		app.Usage(os.Args[1:])
-		os.Exit(2)
+	debug := os.Getenv("DEBUG") != ""
+	if debug {
+		log.Println("displaying debug logs")
 	}
 
-	if *debug {
+	h := os.Getenv("HDOP")
+	HDOP := float64(-1)
+	if h != "" {
+		h, err := strconv.ParseFloat(h, 64)
+		if err != nil {
+			log.Fatalf("uanble to parse HDOP err:%v", err)
+		}
+		HDOP = h
+		log.Println("set to skip gps data with  accuracy below HDOP:", HDOP)
+	}
+	appKey := os.Getenv("app_key")
+	if appKey == "" {
+		log.Fatal("missing app_key env variable")
+	}
+	if len(appKey) != 32 {
+		log.Fatalf("app_kee should be 32 char long, app key length:%v", len(appKey))
+	}
+	devEUI := os.Getenv("dev_eui")
+	if appKey == "" {
+		log.Fatal("missing dev_eui env variable")
+	}
+	if len(devEUI) != 16 {
+		log.Fatalf("dev_eui should be 16 char long, app key length:%v", len(devEUI))
+	}
+
+	if debug {
 		log.Println("enabling gps module")
 	}
-	respChan, err := enableGPS(*debug)
+	respChan, err := startGPS(debug, HDOP)
 	if err != nil {
 		log.Fatal("failed to enable gps err:", err)
 	}
 
-	if *debug {
+	if debug {
 		log.Println("enabling lora module")
 	}
-	lora, err := newLoraConnection(*devEUI, *appKey, *debug)
+	lora, err := newLoraConnection(devEUI, appKey, debug)
 	if err != nil {
 		log.Fatal("failed to create lora connection err:", err)
 	}
 
+	attempt := 1
 	for {
 		dataGPS := <-respChan
 		dataLora := hex.EncodeToString([]byte(strconv.FormatFloat(dataGPS.Latitude, 'f', -1, 64) + "," + strconv.FormatFloat(dataGPS.Longitude, 'f', -1, 64)))
-
-		log.Println("sending data", dataGPS, "len", len(dataLora))
-		_, err := lora.Send("0,1," + dataLora)
+		if debug {
+			log.Printf("%v:trying to send the gps data:%v \n", attempt, dataGPS)
+		}
+		resp, err := lora.Send("0,1," + dataLora)
 		if err != nil {
 			log.Println("failed to send data err:", err)
 			// Attempt to register again.
-			log.Println("attempting to register again")
-			lora, _ = newLoraConnection(*devEUI, *appKey, *debug)
+			log.Println(attempt, ":registration retry")
+			attempt++
+			lora, _ = newLoraConnection(devEUI, appKey, debug)
+			continue
 		}
 
+		if resp == rak811.STATUS_TX_COMFIRMED {
+			log.Println("STATUS_TX_COMFIRMED send response")
+			attempt = 1
+		}
+		if resp == rak811.STATUS_TX_UNCOMFIRMED {
+			log.Println("unconfirmed data sent data:", dataLora)
+			attempt = 1
+		}
 	}
 }
 
-func enableGPS(debug bool) (chan nmea.RMC, error) {
+func startGPS(debug bool, HDOP float64) (chan nmea.GGA, error) {
+	respChan := make(chan nmea.GGA)
+	go func() {
+		reader, err := setupGPS(debug)
+		if err != nil {
+			log.Fatalf("initial gps module setup err:%v", err)
+		}
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Println("reading gps serial err:", err)
+				reader, err = setupGPS(debug)
+				if err != nil {
+					log.Fatalf("gps setup retry after a failed read err:%v", err)
+				}
+			}
+			line = strings.TrimSpace(line)
+			parsed, err := nmea.Parse(line)
+			if err != nil {
+				log.Println("unable to parse GPS response line:", line, " err:", err)
+				continue
+			}
+			if parsed.DataType() == nmea.TypeGGA {
+				dataGPS := parsed.(nmea.GGA)
+				// Send only GPS data if it is valid.
+				if dataGPS.FixQuality == nmea.Invalid {
+					if debug {
+						log.Println("skip sending invalid GPS data", dataGPS)
+					}
+					continue
+				}
+				// When HDOP precision is set,
+				// skip sending anything below the pricions level.
+				if HDOP != -1 {
+					if dataGPS.HDOP >= HDOP {
+						if debug {
+							log.Printf("skip sending low accuracy GPS accuracy threshold:%v  data:%v", HDOP, dataGPS)
+						}
+						continue
+					}
+				}
+				select {
+				case respChan <- dataGPS: // Don't block when the reciver is not ready.
+				default:
+				}
+			}
+		}
+	}()
+
+	return respChan, nil
+}
+
+func setupGPS(debug bool) (*bufio.Reader, error) {
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600, ReadTimeout: 3000 * time.Second}
 	s, err := serial.OpenPort(c)
 	if err != nil {
@@ -82,8 +156,8 @@ func enableGPS(debug bool) (chan nmea.RMC, error) {
 	reader := bufio.NewReader(s)
 
 	// Full ref: https://cdn-shop.adafruit.com/datasheets/PMTK_A08.pdf
-	// Turn on just minimum info (RMC only, location):
-	command := "PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+	// Turn on GGA:
+	command := "PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
 	s.Write([]byte("$" + command + "*" + XORChecksum(command) + "\r\n"))
 
 	if !gerMTKAck(debug, 314, reader) {
@@ -96,41 +170,12 @@ func enableGPS(debug bool) (chan nmea.RMC, error) {
 	if !gerMTKAck(debug, 220, reader) {
 		return nil, errors.New("no cmd ack")
 	}
-
-	respChan := make(chan nmea.RMC)
-	go func() {
-		for {
-			line, err := reader.ReadString('\n')
-			if err != nil {
-				log.Fatalf("reading gps serial err:%v", err)
-			}
-			line = strings.TrimSpace(line)
-			parsed, err := nmea.Parse(line)
-			if err != nil {
-				log.Println("unable to parse GPS response line:", line, " err:", err)
-				continue
-			}
-			if parsed.DataType() == nmea.TypeRMC {
-				dataGPS := parsed.(nmea.RMC)
-				// Send only GPS data if it is valid.
-				if dataGPS.Validity != nmea.ValidRMC {
-					if debug {
-						log.Println("skip sending invalid GPS data", dataGPS)
-					}
-					continue
-				}
-				respChan <- dataGPS
-			}
-		}
-	}()
-
-	return respChan, nil
+	return reader, nil
 }
 
 func newLoraConnection(devEUI, appKey string, debug bool) (*rak811.Lora, error) {
 	cfg := &serial.Config{
-		ReadTimeout: 55 * time.Second,
-		Name:        "/dev/ttyAMA0", // Inside docker /dev/serial0 is not available even in priviliged.
+		Name: "/dev/ttyAMA0", // Inside docker /dev/serial0 is not available even in priviliged.
 	}
 	lora, err := rak811.New(cfg)
 	if err != nil {
@@ -165,13 +210,16 @@ func newLoraConnection(devEUI, appKey string, debug bool) (*rak811.Lora, error) 
 	attempt := 1
 	for {
 		now := time.Now()
-		resp, err = lora.JoinOTAA(10 * time.Minute)
+		if debug {
+			log.Print("sending join request attempt:", attempt)
+		}
+		resp, err = lora.JoinOTAA()
 		if err != nil {
-			log.Println("Reseting the modile due to a join request err:", err)
+			log.Println("Reseting the module due to a join request err:", err)
 			newLoraConnection(devEUI, appKey, debug)
 		}
 
-		if resp == rak811.JoinSuccess {
+		if resp == rak811.STATUS_JOINED_SUCCESS {
 			log.Println("lora module joined, request duration:", time.Since(now))
 			break
 		}
@@ -199,7 +247,7 @@ func gerMTKAck(debug bool, cmdID int64, reader *bufio.Reader) bool {
 		}
 		resp, err := nmea.Parse(strings.TrimSpace(line))
 		if err != nil {
-			log.Fatal("parsing the response err:", err)
+			log.Fatal("parsing the response:", line, "err:", err)
 		}
 		if resp.TalkerID() == nmea.TypeMTK {
 			d := resp.(nmea.MTK)
