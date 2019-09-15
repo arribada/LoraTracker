91ec505c67d8d0a2609d3f0a3401778e8002614c
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-09-15 13:21:06 +0300

    added metrics for distance from gateway in meters
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- receiver/LoraToConnect/main.go
+++ receiver/LoraToConnect/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"mime/multipart"
 	"net/http"
 	"net/url"
@@ -28,10 +29,10 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var coordinates = promauto.NewGaugeVec(
+var distanceMeters = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{
-		Name: "coordinates",
-		Help: "GPS longitude and altitude.",
+		Name: "distance_meters",
+		Help: "Distance in meters between the received gps coordinates and the gaetway location.",
 	},
 	[]string{"vector"},
 )
@@ -178,8 +179,15 @@ func (s *Handler) createAlert(w http.ResponseWriter, r *http.Request, data *Data
 	if err != nil {
 		return err
 	}
-	coordinates.WithLabelValues("latitude").Set(lat)
-	coordinates.WithLabelValues("longitude").Set(long)
+
+	if len(data.RXInfo) == 0 {
+		log.Println("received lora data doesn't include gateway meta data")
+	} else {
+		// Distance from each gateway that received this data.
+		for _, gwMeta := range data.RXInfo {
+			distanceMeters.WithLabelValues("gateway_id").Set(distance(lat, long, gwMeta.Location.Latitude, gwMeta.Location.Longitude, "M"))
+		}
+	}
 	devID := genDevID(data)
 	alertID, ok := s.alertIDBuf[devID]
 	if !ok {
@@ -556,4 +564,34 @@ type SMARTAlertType struct {
 	UUID     string `json:"uuid"`
 	TypeUUID string `json:"typeUuid"`
 	Label    string `json:"label"`
+}
+
+func distance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, unit ...string) float64 {
+	const PI float64 = 3.141592653589793
+
+	radlat1 := float64(PI * lat1 / 180)
+	radlat2 := float64(PI * lat2 / 180)
+
+	theta := float64(lng1 - lng2)
+	radtheta := float64(PI * theta / 180)
+
+	dist := math.Sin(radlat1)*math.Sin(radlat2) + math.Cos(radlat1)*math.Cos(radlat2)*math.Cos(radtheta)
+
+	if dist > 1 {
+		dist = 1
+	}
+
+	dist = math.Acos(dist)
+	dist = dist * 180 / PI
+	dist = dist * 60 * 1.1515
+
+	if len(unit) > 0 {
+		if unit[0] == "K" {
+			dist = dist * 1.609344
+		} else if unit[0] == "N" {
+			dist = dist * 0.8684
+		}
+	}
+
+	return dist
 }
