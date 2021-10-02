38366513b83edcd02e1dfcfe0819b25c4b185de8
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2021-10-02 11:21:26 +0300

    separate settings for rhino and lion tag
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

---
+++ configs/commands.js
@@ -0,0 +1,14 @@
+// This is usefull if the device doesn't acquire GPS.
+// port 99
+{
+    "command": {
+        "reset": true
+    }
+}
+// port 99   
+// Check the current settings.      
+{
+    "command": {
+        "send_settings": true
+    }
+}
